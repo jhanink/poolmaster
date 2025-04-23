db16402666c85ea58bf48b82720b33b3bf8e5fa7
Joe Hanink <jhanink@gmail.com>
2025-04-23 11:28:26 -0700

    add jest

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -5,8 +5,7 @@ const appUrl = 'http://localhost:5173';
 test('has title', async ({ page }) => {
   await page.goto(appUrl);
 
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Pool Hall Master/);
-  const appTitle = page.getByText('South Dakota Billiards');
-  await expect(appTitle).toBeVisible();
+  const defaultVenueName = page.getByText('South Dakota Billiards');
+  await expect(defaultVenueName).toBeVisible();
 });
