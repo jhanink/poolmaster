1da69a6fd0ad08abf12816b50bb3cb20ec6b661f
Joe Hanink <jhanink@gmail.com>
2025-05-12 12:50:28 -0700

    fix misnamed field names

--- app/routes/components/mainTakeover/admin/adminTables.tsx
+++ app/routes/components/mainTakeover/admin/adminTables.tsx
@@ -64,7 +64,7 @@ export default function AdminTables() {
       name: `Table ${number}`,
       tableTypeId: DefaultTableTypeData.id,
       tableRateId: DEFAULT_TABLE_RATE_ID,
-      ignoreTableTypeRateRate: false,
+      ignoreTableTypeRate: false,
       isActive: true,
       forDelete: false,
       forAdd: true,
