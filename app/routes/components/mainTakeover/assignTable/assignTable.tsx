e1a85d8991f92726a996fa9df7feca1d60698073
Joe Hanink <jhanink@gmail.com>
2025-05-12 11:33:37 -0700

    finish renaming of mainTakeover components

---
+++ app/routes/components/mainTakeover/assignTable/assignTable.tsx
@@ -0,0 +1,98 @@
+import { useEffect, useRef } from "react";
+import { useAtom } from "jotai";
+import { appStateAtom, mainTakoverAtom, selectedTableAtom } from "~/appStateGlobal/atoms";
+import { type TableItem } from "~/config/AppState";
+import { AppStorage } from "~/util/AppStorage";
+import { actionButtonStyles } from "~/util/GlobalStylesUtil";
+import { Helpers } from "~/util/Helpers";
+import { fragmentExitTakeover } from "../../fragments/fragments";
+
+const unassignedStyle = `inline-block m-1 mb-2 rounded-full py-1 px-4 text-xs border border-gray-700 text-gray-600 font-bold hover:cursor-pointer`;
+
+export default function AssignTable() {
+  const [APP_STATE, setAppState] = useAtom(appStateAtom);
+  const [, setSelectedTable] = useAtom(selectedTableAtom);
+  const [MAIN_TAKEOVER, setMainTakeover] = useAtom(mainTakoverAtom);
+
+  const TopRef = useRef<HTMLDivElement>(null);
+  const tables = APP_STATE.tables.filter((table: TableItem) => {
+    return table.isActive;
+  });
+
+  const onClickTableChip = async (event: React.MouseEvent<HTMLDivElement>, table: TableItem) => {
+    event.stopPropagation();
+    event.preventDefault();
+    const tableId = table.id
+    const guestId = MAIN_TAKEOVER.assignTable?.id || 0;
+    const newState = await AppStorage.assignToTableRemote({tableId, guestId});
+    setAppState(newState);
+    reset();
+  }
+
+  const onClickCancelAssign = () => {
+    setMainTakeover(undefined);
+    setSelectedTable(undefined);
+  };
+
+  const reset = () => {
+    setSelectedTable(undefined);
+    setMainTakeover(undefined);
+  }
+
+  useEffect(() => {
+    TopRef.current && TopRef.current.scrollIntoView();
+  },)
+
+  return (
+    <div className="select-none flex-1 text-center relative items-center" ref={TopRef}>
+      {fragmentExitTakeover(onClickCancelAssign)}
+      <div>
+        <div className="CONTENT mx-5">
+          {!Helpers.tablesAvailable(APP_STATE).length && <>
+            <div className="text-gray-500 text-sm mt-3 p-3 uppercase">
+              No open tables available
+            </div>
+            <button className={`${actionButtonStyles}`} onClick={onClickCancelAssign}>Exit</button>
+          </>}
+          {!!Helpers.tablesAvailable(APP_STATE).length && <>
+            <div className="text-2xl mt-5">
+              <div className="text-gray-300 uppercase">
+                {!!MAIN_TAKEOVER?.assignTable?.assignedAt && (
+                  <span>Move</span>
+                )}
+                {!MAIN_TAKEOVER?.assignTable?.assignedAt && (
+                  <span>Assign</span>
+                )}
+                <span className="ml-2">Guest</span>
+              </div>
+              <div className="uppercase text-red-500 mx-3 my-5 whitespace-nowrap">
+                {MAIN_TAKEOVER.assignTable?.name}
+              </div>
+              <div className="text-gray-400 text-base">
+                to one of the following open tables
+              </div>
+            </div>
+            <div className="mt-5 inline-block max-w-lg mx-auto">
+              {
+                tables
+                  .filter((table: TableItem) => !table.guest)
+                  .sort((A: TableItem, B: TableItem) =>
+                    A.number - B.number
+                  )
+                  .map((table: TableItem) =>
+                    <div className={`CHIP ${unassignedStyle}`}
+                      key={table.id}
+                      data-table-id={table.id}
+                      onClick={(event) => onClickTableChip(event, table)}
+                    >
+                      {table.nickname || table.name}
+                    </div>
+                  )
+              }
+            </div>
+          </>}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/routes/components/mainTakeover/tableCloseout/tableCloseout.tsx
@@ -0,0 +1,263 @@
+import { useEffect, useState, useRef } from "react";
+import { useAtom } from "jotai";
+import { appStateAtom, mainTakoverAtom, selectedTableAtom } from "~/appStateGlobal/atoms";
+import { AppStorage } from "~/util/AppStorage";
+import { actionButtonStyles, formFieldStyles, optionStyles } from "~/util/GlobalStylesUtil";
+import { Helpers, type TimeElapsed } from "~/util/Helpers";
+import ModalConfirm from "../../ui-components/modal/modalConfirm";
+import { fragmentExitTakeover } from "../../fragments/fragments";
+import { type TableItem, type TableRate } from "~/config/AppState";
+
+type BillablePlayer = {
+  id: number,
+  name: string,
+  hours: string,
+  rate: string,
+  billable: boolean,
+}
+type BillableData = {
+  players: BillablePlayer[],
+}
+
+export default function TableCloseout() {
+  const [APP_STATE, setAppState] = useAtom(appStateAtom);
+  const [, setSelectedTable] = useAtom(selectedTableAtom);
+  const [MAIN_TAKEOVER, setMainTakeover] = useAtom(mainTakoverAtom);
+  const [, setElapsedTime] = useState<TimeElapsed>({} as TimeElapsed);
+  const [SHOW_CONFIRM_CLOSEOUT, setShowConfirmCloseout] = useState(false);
+
+  const [HOURS_DATA, setHoursData] = useState('');
+  const [BILLABLE_DATA, setBillableData] = useState<BillableData>({} as BillableData);
+  const [SELECTED_RATE, setSelectedRate] = useState<TableRate>({} as TableRate);
+
+  const TopRef = useRef<HTMLDivElement>(null);
+
+  const onClickCancelCheckout = () => {
+    MAIN_TAKEOVER.closeoutTable.guest.closedOutAt = 0;
+    setMainTakeover(undefined);
+  }
+
+  const setupBillablePlayers = (hours: string) => {
+    const table: TableItem = MAIN_TAKEOVER.closeoutTable;
+    let tableRate: TableRate = APP_STATE.tableRates.find((rate) => (rate.id === table.tableRateId) && rate.isActive);
+    if (!tableRate) {
+      tableRate = APP_STATE.tableRates[0];
+      table.tableRateId = tableRate.id;
+    }
+    setSelectedRate(tableRate);
+    const rate = tableRate.tableRateRules.hourlyRate;
+    const guest = table.guest;
+    const PLAYERS_COUNT = Math.max(guest.partySize, guest.extraPlayers.length + 1);
+    const players = []
+    for (let i = 0; i < (PLAYERS_COUNT); i++) {
+      players.push({
+        id: i,
+        name: `Player ${i + 1}`,
+        hours,
+        rate,
+        billable: true,
+      });
+    }
+    players[0].name = guest.name.toUpperCase();
+
+    guest.extraPlayers.forEach((player, index) => {
+      const playersIndex = index + 1;
+      if ((playersIndex) > players.length - 1) return;
+      players[playersIndex].name = player.name.toUpperCase();
+      const time = Helpers.timeElapsed(player.assignedAt, guest.closedOutAt);
+      players[playersIndex].hours = `${time.durationHoursDecimal3}`;
+    });
+
+    setBillableData({players});
+  }
+
+  const onChangeTableRate = (event) => {
+    const selectedRate: TableRate = APP_STATE.tableRates.find((rate) => rate.id === Number(event.target.value));
+    setSelectedRate(selectedRate);
+    const players = BILLABLE_DATA.players.map((player) => {
+      player.rate = selectedRate.tableRateRules.hourlyRate;
+      return player;
+    });
+    setBillableData({...BILLABLE_DATA, players});
+  }
+
+  const useTableHours = (hours: string) => {
+    BILLABLE_DATA.players.forEach((player) => {
+      player.hours = hours;
+    })
+    setBillableData({...BILLABLE_DATA});
+  }
+
+  const playersTotal = () => {
+    let total = 0;
+    BILLABLE_DATA.players?.forEach((player) => {
+      if (player.billable) {
+        total += Number(player.hours) * Number(player.rate);
+      }
+    })
+    return total.toFixed(2);
+  }
+
+  const playerAssignedAt = (player: BillablePlayer, index: number) => {
+    const guest = MAIN_TAKEOVER.closeoutTable.guest;
+    const assignedAt = !index ? guest.assignedAt : guest.extraPlayers[index - 1].assignedAt;
+    return new Date(assignedAt).toLocaleString();
+  }
+
+  const onClickReset = () => {
+    const table = MAIN_TAKEOVER.closeoutTable;
+    const guest = table.guest;
+    const start = guest.assignedAt;
+    const end = guest.closedOutAt;
+    const hours = Helpers.timeElapsed(start, end);
+    const selectedRate: TableRate = APP_STATE.tableRates.find((rate) => table.tableRateId);
+    setElapsedTime(hours);
+    setHoursData(`${hours.durationHoursDecimal3}`);
+    setSelectedRate(selectedRate);
+    setupBillablePlayers(hours.durationHoursDecimal3);
+  }
+
+  const onClickFinalConfirm = async () => {
+    const newAppState = await AppStorage.deleteGuestRemote(MAIN_TAKEOVER.closeoutTable.guest.id);
+    setAppState(newAppState);
+    setSelectedTable(undefined);
+    setMainTakeover(undefined);
+  }
+
+  useEffect(() => {
+    onClickReset();
+    TopRef.current && TopRef.current.scrollIntoView();
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-center items-center text-center bg-black border-white select-none" ref={TopRef}>
+      {fragmentExitTakeover(onClickCancelCheckout)}
+      <div className="CONTENT flex-1 text-center">
+        <div className="text-gray-400 mt-5">
+          <div className="text-2xl mb-3">
+            <span className="text-green-500">{MAIN_TAKEOVER.closeoutTable.name}</span>
+          </div>
+          <div className="inline-block text-right">
+            <div className="TIME flex items-center text-base mb-2">
+              <div className="w-[50px]">
+                Hours
+              </div>
+              <div className="inline-block">
+              <div className="inline-block w-[90px]">
+                <input
+                  type="text"
+                  className={`text-gray-500 w-[70px] ${formFieldStyles}`}
+                  maxLength={6}
+                  onChange={(event) => {
+                    const hours = event.target.value.trim()
+                    setHoursData(hours);
+                  }}
+                  value={`${HOURS_DATA}`}
+                />
+                </div>
+                <span className={actionButtonStyles} onClick={() => {useTableHours(HOURS_DATA)}}>use</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="mt-2">
+            <select
+              onChange={onChangeTableRate}
+              value={SELECTED_RATE.id}
+              className={`${formFieldStyles}`}
+            >
+              {APP_STATE.tableRates
+                .filter((rate) => rate.isActive)
+                .map((rate) => (
+                <option key={rate.id} className={optionStyles} value={rate.id}>{rate.name}</option>
+              ))}
+            </select>
+          </div>
+        </div>
+
+        <div className="WORKSHEET text-left border border-gray-800 p-5 mx-5 mt-5">
+          {BILLABLE_DATA.players?.map((player, index) => (
+            <div className="PLAYER mb-4" key={player.id}>
+              <div>
+                <div className="inline-block text-gray-500 mr-3">
+                  <input
+                    type="checkbox"
+                    checked={player.billable}
+                    onChange={(event) => {
+                      player.billable = event.target.checked;
+                      setBillableData({...BILLABLE_DATA});
+                    }}
+                  />
+                </div>
+                <div className={`inline-block text-base ${index > MAIN_TAKEOVER.closeoutTable.guest.extraPlayers.length ? 'text-gray-300 italic' : 'text-blue-400'}`}>
+                  {player.name}
+                  {index === 0 && (
+                    <div className="inline-block">&nbsp; (Main Player)</div>
+                  )}
+                </div>
+              </div>
+              <div className="text-sm text-gray-500 ml-7">
+                {playerAssignedAt(player, index)}
+              </div>
+              <div className="text-sm ml-7 mt-2">
+                <div className="inline-block w-[90px]">
+                  <div className="text-xs text-gray-500">HOURS</div>
+                  <input
+                  type="text"
+                    className={`w-[75px] ${formFieldStyles}`}
+                    onChange={(event) => {
+                      player.hours = event.target.value;
+                      setBillableData({...BILLABLE_DATA});
+                    }}
+                    value={`${player.hours}`}
+                  />
+                </div>
+                <div className="inline-block w-[90px]">
+                  <div className="text-xs text-gray-500">RATE</div>
+                  <input
+                    type="text"
+                    className={`w-[75px] ${formFieldStyles}`}
+                    maxLength={6}
+                    onChange={(event) => {
+                      player.rate = event.target.value;
+                      setBillableData({...BILLABLE_DATA});
+                    }}
+                    value={`${player.rate}`}
+                  />
+                </div>
+                <div className="inline-block w-[90px] text-gray-500">
+                = &nbsp;
+                  <div className="inline-block text-base text-green-500">
+                    ${(Number(player.hours) * Number(player.rate)).toFixed(2)}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="text-2xl text-gray-400 my-3">
+          Total Bill: &nbsp;
+          <span className="text-green-500 text-2xl">${playersTotal()}</span>
+        </div>
+
+        <div className="my-3 mb-20">
+          <button className={`${actionButtonStyles} mx-2`} onClick={onClickReset}>Reset</button>
+          <button className={`${actionButtonStyles} mx-2`} onClick={() => {setShowConfirmCloseout(true)}}>Close Out {MAIN_TAKEOVER.closeoutTable.name}</button>
+        </div>
+      </div>
+      <ModalConfirm
+        show={SHOW_CONFIRM_CLOSEOUT}
+        dialogTitle={`CLOSE OUT ${MAIN_TAKEOVER.closeoutTable.name}`}
+        dialogMessageFn={() => <div className="text-base">
+          <div className="mt-3 text-xl text-blue-500">{MAIN_TAKEOVER.closeoutTable.guest.name.toUpperCase()}</div>
+          <div className="mt-2 text-2xl">Total: <span className="text-green-500">${playersTotal()}</span></div>
+        </div>}
+        onConfirm={onClickFinalConfirm}
+        onCancel={() => {setShowConfirmCloseout(false)}}
+      />
+    </div>
+  );
+}
