ac2ecfde30aca410a8fb486d14f8876208dd609d
Joe Hanink <jhanink@gmail.com>
2025-06-04 17:25:51 -0700

    Admin Cog: prevent TakeOver EXIT from Admin Cog. User must click EXIT

--- app/routes/components/brandingBar/brandingBar.tsx
+++ app/routes/components/brandingBar/brandingBar.tsx
@@ -3,26 +3,25 @@ import { useAtom } from "jotai";
 import { mainTakoverAtom, selectedTableAtom } from "~/appStateGlobal/atoms";
 import { actionIconStyles } from "~/util/GlobalStylesUtil";
 
-const footerStyles = `flex items-center justify-center !text-xs mb-0 mt-2`;
-const adminCogStyles = `size-[16px] relative top-[1px] hover:text-yellow-500 text-gray-500`;
+const footerStyles = `flex items-center justify-center !text-xs mb-0 mt-2 select-none`;
+const adminCogStyles = `size-[16px] relative top-[1px] hover:text-white text-gray-500`;
 
 export default function BrandingBar() {
   const [, setSelectedTable] = useAtom(selectedTableAtom);
   const [MAIN_TAKEOVER, setMainTakeover] = useAtom(mainTakoverAtom);
 
-  const onClickSettings = (event: React.MouseEvent<HTMLDivElement>) => {
-
-    event.preventDefault();
-    event.stopPropagation();
+  const onClickSettings = () => {
+    if (!!MAIN_TAKEOVER) return;
     setSelectedTable(undefined);
-    setMainTakeover(MAIN_TAKEOVER ? undefined : {adminScreen: true});
+    setMainTakeover({adminScreen: true});
   }
 
   return (
     <div className={`${footerStyles}`}>
-      <span className={`inline-block ${actionIconStyles}`}
+      <span
+        className={`inline-block ${actionIconStyles}`}
         onClick={onClickSettings}>
-        <CogIcon className={`${adminCogStyles} ${MAIN_TAKEOVER?.adminScreen && 'text-yellow-500'}`}></CogIcon>
+        <CogIcon className={`${adminCogStyles} ${MAIN_TAKEOVER?.adminScreen && 'text-white'}`}></CogIcon>
       </span>
       <span className="text-gray-600 ml-1">POOLHALL</span>
       <span className="text-red-500">MASTER</span>
--- app/routes/components/header/header.tsx
+++ app/routes/components/header/header.tsx
@@ -16,7 +16,7 @@ const filterStyle = `inline-block py-1 px-2 mx-2 border border-gray-800 rounded-
 const dndTargetBaseStyle = `flex justify-center text-gray-500 py-2 pb-2 text-sm select-none`;
 const dndActiveStyle = `bg-green-300 invert`;
 const dndOverStyle = `blur-[2px] bg-white`;
-const headerStyles = `flex items-center justify-center select-none mx-2 text-nowrap text-lg text-slate-400 rounded-full bg-gray-900 mt-2 py-1 mb-1`;
+const headerStyles = `flex items-center justify-center select-none text-nowrap text-lg text-slate-400 rounded-full bg-gray-900 mt-2 py-1 mb-1`;
 
 export default function AppHeader() {
   const [APP_STATE] = useAtom(appStateAtom);
@@ -59,9 +59,9 @@ export default function AppHeader() {
           className={`text-lg ${dndTargetBaseStyle} ${canDrop &&  (isOver ? dndOverStyle : dndActiveStyle)}`}>
           {!(SELECTED_LIST_FILTER === 'tablelist') && (
             <div className="flex flex-col items-center justify-center">
-              <div className={`${SELECTED_LIST_FILTER === 'waitlist' && selectedFilterStyle} ${filterStyle} text-blue-500`} onClick={(event) => onClickListFilter('waitlist')}>
+              <div className={`${SELECTED_LIST_FILTER === 'waitlist' && selectedFilterStyle} ${filterStyle} !mx-1 text-blue-500`} onClick={(event) => onClickListFilter('waitlist')}>
                 <span className={`${statusPillStyles} text-nowrap`}>
-                  {APP_STATE.guestList.length} <span className="ml-1 capitalize">{Helpers.pluralizeGuestsWaiting(APP_STATE)} In Line</span>
+                  {APP_STATE.guestList.length} <span className="ml-1 capitalize">Pending</span>
                 </span>
               </div>
               <div className="mt-2 text-gray-500">Est Wait: {Helpers.averageWaitTime(APP_STATE)}</div>
@@ -69,9 +69,9 @@ export default function AppHeader() {
           )}
           {!(SELECTED_LIST_FILTER === 'waitlist') && (
             <div className="flex flex-col items-center justify-center">
-              <div className={`${SELECTED_LIST_FILTER === 'tablelist' && selectedFilterStyle} ${filterStyle} text-green-500`} onClick={(event) => onClickListFilter('tablelist')}>
+              <div className={`${SELECTED_LIST_FILTER === 'tablelist' && selectedFilterStyle} ${filterStyle} !mx-1 text-green-500`} onClick={(event) => onClickListFilter('tablelist')}>
                 <span className={`${statusPillStyles}`}>
-                  {Helpers.tablesAssigned(APP_STATE).length} <span className="ml-1 capitalize">{Helpers.pluralizeTablesAssigned(APP_STATE)} Used - {Helpers.percentTablesAssigned(APP_STATE)}%</span>
+                  {Helpers.tablesAssigned(APP_STATE).length} <span className="ml-1 capitalize">Active - {Helpers.percentTablesAssigned(APP_STATE)}%</span>
                 </span>
               </div>
               <div className="mt-2 text-gray-500"> &nbsp; Open Tables: {Helpers.tablesAvailable(APP_STATE).length}</div>
