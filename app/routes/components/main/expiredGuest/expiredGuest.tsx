a98d31cf807459281def526ddd53d7cb1be2ff4c
Joe Hanink <jhanink@gmail.com>
2025-06-12 20:59:50 -0700

    ExpiredGuest: modify date-time format. Also fix admin width

--- app/routes/components/main/expiredGuest/expiredGuest.tsx
+++ app/routes/components/main/expiredGuest/expiredGuest.tsx
@@ -3,20 +3,22 @@ import { appStateAtom, isSavingAtom } from "~/appStateGlobal/atoms";
 import type { Guest } from "~/config/AppState";
 import { AppStorage } from "~/util/AppStorage";
 import { actionButtonStyles } from "~/util/GlobalStylesUtil";
+import dayjs from "dayjs";
 
 export default function ExpiredGuest(props: {
   guest: Guest,
 }) {
   const [, setAppState] = useAtom(appStateAtom);
   const [SAVING, setSaving] = useAtom(isSavingAtom);
+  const CREATED_AT = dayjs(props.guest.createdAt).format('MMM D - H:m a');
   return (<>
     <div className={``}>
       <div className={`my-3 text-gray-500`}>
         <div className="text-sm mb-2">
           <span className="italic text-lg">Expired Guest</span>: <span className="text-cyan-500 uppercase ml-2">{props.guest.name}</span>
         </div>
-        <div className="italic text-xs mb-5">
-           {props.guest.assignedAt ? 'Assigned' : 'Added'} more than a day ago
+        <div className="italic text-sm mb-5">
+           Created: <span className="text-cyan-500 ml-2">{CREATED_AT}</span>
         </div>
       </div>
       <div className={'mb-3'}>
