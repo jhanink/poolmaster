5555ef5210d12941677c67828d87fd6f164a188a
Joe Hanink <jhanink@gmail.com>
2025-07-01 11:55:19 -0700

    main lists constraing to 450px

--- app/util/GlobalStylesUtil.ts
+++ app/util/GlobalStylesUtil.ts
@@ -49,7 +49,7 @@ export const separatorBarStyles = `border-b border-gray-900 mx-1`;
 
 export const columnBorders = `${FeatureFlags.SHOW_MAIN_SWIMLANES && 'md:border border-gray-900 rounded-xl sm:mx-auto md:mx-0 md'}`;
 export const mainListSwimLaneHeader = `sticky bg-gray-900 top-[-1px] z-9 md:flex hidden border-b border-gray-950 p-2 text-xl items-center text-slate-400 rounded-t-xl`;
-export const mainColumnStyles = `flex-1 text-center select-none min-w-[345px] max-w-[600px] ${columnBorders}`;
+export const mainColumnStyles = `flex-1 text-center select-none min-w-[345px] max-w-[450px] ${columnBorders}`;
 export const mainColumnContentStyles = `mt-3 px-1 md:px-2`;
 export const columnItemListStyles = `flex flex-col gap-2`;
 
