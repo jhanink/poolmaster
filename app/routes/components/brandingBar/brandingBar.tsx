c102bde6709e8496ed9ccd7646441f4551f645fa
Joe Hanink <jhanink@gmail.com>
2025-07-05 10:36:46 -0700

    Branding Bar: make the whole branding bar clickable with HomeButton. Increase vertical padding to improve touch target

--- app/routes/components/brandingBar/brandingBar.tsx
+++ app/routes/components/brandingBar/brandingBar.tsx
@@ -2,25 +2,22 @@ import { HomeIcon } from "@heroicons/react/24/outline";
 import { useAtom } from "jotai";
 import { mainTakoverAtom } from "~/appStateGlobal/atoms";
 
-const brandStyles = `flex items-center justify-center !text-xs mb-0 mt-2 select-none`;
+const brandStyles = `flex items-center justify-center !text-xs mb-1 mt-3 select-none`;
 const headerActionIconStyles = `size-[16px] hover:text-white hover:cursor-pointer text-gray-500`;
 
 export default function BrandingBar() {
-  const [MAIN_TAKEOVER, setMainTakeover] = useAtom(mainTakoverAtom);
+  const [, setMainTakeover] = useAtom(mainTakoverAtom);
 
   const onClickHome = async () => {
     setMainTakeover({homeRedirect: true});
   }
 
   return (
-    <div className={`${brandStyles}`}>
+    <div className={`${brandStyles} hover:cursor-pointer`} onClick={onClickHome}>
       <div className="text-gray-600 ml-1">POOLHALL</div>
       <div className="text-gray-400">MASTER</div>
       <div className="text-gray-600 ml-2">2025</div>
-      <div
-        className={`ml-2`}
-        onClick={onClickHome}
-      >
+      <div className={`ml-2`}>
         <HomeIcon className={`${headerActionIconStyles}`} title="Refresh"/>
       </div>
     </div>
