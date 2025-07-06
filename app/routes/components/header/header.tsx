8b4f342095bceeb1e41b4eca4d37f6de17f55b65
Joe Hanink <jhanink@gmail.com>
2025-07-06 12:42:25 -0700

    FIX header alignments

--- app/routes/components/header/header.tsx
+++ app/routes/components/header/header.tsx
@@ -107,23 +107,23 @@ export default function AppHeader() {
 
   const fragmentListFilters = () => {
     return (<>
-      <div className={`flex items-center justify-center md:scale-125`}>
+      <div className={`flex justify-center md:scale-125`}>
+        {!QUIET_MODE && (
+          <div className="block md:hidden size-6 text-gray-400 mt-[2px] mr-2 hover:cursor-pointer hover:text-white"
+            onClick={(event) => setMainTakeover({addGuest: true})}>
+            <PlusIcon></PlusIcon>
+          </div>
+        )}
         {(SELECTED_LIST_FILTER !== 'tablelist') && (
           <div className="flex flex-col items-center justify-center">
             <div className="flex items-center">
-              {!QUIET_MODE && (
-                <div className="block md:hidden size-6 text-gray-400 mr-2 hover:cursor-pointer hover:text-white"
-                  onClick={(event) => setMainTakeover({addGuest: true})}>
-                  <PlusIcon></PlusIcon>
-                </div>
-              )}
               <div className={`${SELECTED_LIST_FILTER === ListFilterTypeEnum.WAITLIST && selectedFilterStyle} ${filterStyle} !mx-1 text-blue-600`} onClick={(event) => onClickListFilter('waitlist')}>
                 <span className={`${statusPillStyles} text-nowrap`}>
                   {APP_STATE.guestList.length} <span className="ml-1 capitalize">Waiting</span>
                 </span>
               </div>
             </div>
-            <div className="mt-2 md:mt-0 text-gray-500 text-xs">Ave - <span className="text-gray-300 text-xs">{Helpers.averageWaitTime(APP_STATE)}</span></div>
+            <div className="mt-2 md:mt-1 text-gray-500 text-xs">Ave - <span className="text-gray-300 text-xs">{Helpers.averageWaitTime(APP_STATE)}</span></div>
           </div>
         )}
         {(SELECTED_LIST_FILTER !== ListFilterTypeEnum.WAITLIST) && (
@@ -133,7 +133,7 @@ export default function AppHeader() {
                 {Helpers.tablesAssigned(APP_STATE).length} <span className="ml-1 capitalize">Active - {Helpers.percentTablesAssigned(APP_STATE)}%</span>
               </span>
             </div>
-            <div className="mt-2 md:mt-0 text-gray-500 text-xs"> &nbsp; Open - <span className="text-gray-300 text-xs">{Helpers.tablesAvailable(APP_STATE).length}</span></div>
+            <div className="mt-2 md:mt-1 text-gray-500 text-xs"> &nbsp; Open - <span className="text-gray-300 text-xs">{Helpers.tablesAvailable(APP_STATE).length}</span></div>
           </div>
         )}
       </div>
