f232736afa78cebbff972ae0351bddc339f2897d
Joe Hanink <jhanink@gmail.com>
2025-07-08 11:07:51 -0700

    remove global zoom

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,6 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
-import { GLOBAL_ZOOM } from "./util/GlobalStylesUtil";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -32,7 +31,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        <div className={`h-dvh flex flex-col w-full mx-auto max-w-[1400px]`} style={GLOBAL_ZOOM}>
+        <div className={`h-dvh flex flex-col w-full mx-auto max-w-[1400px]`}>
           {children}
         </div>
         <ScrollRestoration />
